6738c04be5a304a43d284cd283ada83cf1b343a5
Thomas Frei <frei@itq.de>
2015-05-28 02:38:03 +0200

    Combined REST, Mongo, GCM - working

--- misc/mongo.js
+++ misc/mongo.js
@@ -4,25 +4,36 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
+// Create the Mongo Schema
+var deviceSchema = mongoose.Schema({
+    regId     : String
+    , date      : { type: Date, default: Date.now }
+});
+var Device = mongoose.model('Device', deviceSchema);
+
+// Connect and save
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+//db.once('open', function (callback) {
+//    // yay!
+//    var NewDevice = new Device({regId: 'asdf1234'});
+//
+//    NewDevice.save(function (err) {
+//        if (err) return console.error(err);
+//        console.log('saved');
+//    });
+//
+//    Device.find({}, function(err, devices){
+//        console.log('find', devices);
+//    });
+//});
+
+
 db.once('open', function (callback) {
-    // yay!
-
-    var deviceSchema = mongoose.Schema({
-        regId     : String
-        , date      : { type: Date, default: Date.now }
-    });
-    var Device = mongoose.model('Device', deviceSchema);
-
-    var NewDevice = new Device({regId: 'asdf1234'});
-
-    NewDevice.save(function (err) {
-        if (err) return console.error(err);
-        console.log('saved');
-    });
-
     Device.find({}, function(err, devices){
-        console.log('find', devices);
+        //console.log('//second find', devices);
+        devices.forEach(function(device,i,arr){
+            console.log(device.regId);
+        });
     });
 });
---
+++ misc/rest-mongoo-gcm.js
@@ -0,0 +1,106 @@
+/**
+ * Created by Thomas on 28.05.2015.
+ */
+var gcm = require('node-gcm');
+var config = require('./../config.js');
+
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+// Create the Mongo Schema
+var deviceSchema = mongoose.Schema({
+    regId     : String
+    , date      : { type: Date, default: Date.now }
+});
+var Device = mongoose.model('Device', deviceSchema);
+
+// Connect
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// TODO: Transform MongoDB Connection to a model so that DB and Server run parallel
+db.once('open', function (callback) {
+    // -----------------------------------------------------------------
+    // REST API
+    var express = require('express');
+    var app = express();
+    var bodyParser = require('body-parser');
+
+    // app.use(bodyParser.json()); // for parsing application/json
+    app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+
+    app.post('/register', function (req, res) {
+        var regId = req.body.regId;
+
+        if (undefined === regId) {
+            // Error
+            res.json({status: "BAD", msg: "No regId-parameter given"});
+        } else {
+            var NewDevice = new Device({regId: regId});
+
+            NewDevice.save(function (err) {
+                if (err) return console.error(err);
+                console.log('saved');
+                res.json({status: "OK", msg: "Your regId: " + regId}); //TODO: scope compatibility?
+            });
+        }
+    });
+
+    app.get('/getRegIds', function (req, res) {
+        // get all devices
+        var regIds = [];
+        Device.find({}, function(err, devices){
+            devices.forEach(function(device,i,arr){
+                regIds.push(device.regId);
+                console.log(device.regId);
+            });
+            console.log(regIds);
+            res.json(JSON.stringify(regIds));
+        });
+    });
+
+    app.post('/pushMessage', function (req, res) {
+        var data = req.body.data;
+
+        if (undefined === data) {
+            // Error
+            res.json({status: "BAD", msg: "No data to push"});
+        } else {
+            // node-gcm Message
+            var message = new gcm.Message({
+                collapseKey: 'collapseKey',
+                delayWhileIdle: true,
+                timeToLive: 3,
+                data: {
+                    mi5: data
+                }
+            });
+
+            var sender = new gcm.Sender(config.GoogleAPI);
+
+            var regIds = [];
+            Device.find({}, function(err, devices){
+                devices.forEach(function(device,i,arr){
+                    regIds.push(device.regId);
+                    console.log(device.regId);
+                });
+                //console.log(regIds);
+
+                sender.sendNoRetry(message, regIds, function(err, result) {
+                    if(err) {
+                        console.error(err);
+                        res.json(err);
+                    } else {
+                        console.log(result);
+                        res.json(result);
+                    }
+                });
+            });
+        }
+    });
+
+    // Start Server on Port 80
+    app.listen(3001);
+    // -----------------------------------------------------------------
+});

5718982fc94c80231ee977bd907ee1f9b341f52a
Oliver Wangler <oliver@wngr.de>
2015-10-14 22:23:43 +0200

    Update startup script to new path.

--- start.sh
+++ start.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-#echo 'Start forever app.js'
-#cd /home/thomas/js/CloudLink
-#echo 'Start updastream-python-mqtt daemon'
-
-
   case "$1" in
     start)
         echo -n "Starting CloudLink"
-	su - thomas -c '/usr/bin/forever start -l /home/thomas/js/CloudLink/log/forever.log -a -e /home/thomas/js/CloudLink/log/err.log -o /home/thomas/js/CloudLink/log/out.log /home/thomas/js/CloudLink/app.js' &>/var/log/CloudLinkStartup.log 
+	su - cloudlink -c '/usr/bin/forever start -l /opt/cloudlink/CloudLink/log/forever.log -a -e /opt/cloudlink/CloudLink/log/err.log -o /opt/cloudlink/CloudLink/log/out.log -p /tmp /opt/cloudlink/CloudLink/app.js' &>/var/log/CloudLinkStartup.log 
         ;;
     stop)
         echo -n "Stopping CloudLink"
-        pkill -f '/usr/bin/nodej[s] /home/thomas/js/CloudLink/app.js'
-	pkill -f '/usr/bin/nodej[s] /usr/lib/node_modules/forever/bin/monitor /home/thomas/js/CloudLink/app.js'
+        pkill -f '/usr/bin/nodej[s] /opt/cloudlink/CloudLink/app.js'
+	pkill -f '/usr/bin/nodej[s] /usr/lib/node_modules/forever/bin/monitor /opt/cloudlink/CloudLink/app.js'
 	;;
     *)
         ## If no parameters are given, print which are avaiable.

03ccead5d51b5abfa8a61fd7f480d208c59bc93c
Thomas Frei <mail@thomasfrei.de>
2015-11-16 22:20:35 +0100

    proposition for Voucher Unit Tests

---
+++ test/test-voucher-database.js
@@ -0,0 +1,33 @@
+var assert = require('chai').assert;
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+
+describe('Voucher', function () {
+  var VoucherDB = require('./../models/database-voucher').instance;
+
+  beforeEach('Clean database and load mock data', function () {
+  });
+
+  describe('Voucher Database', function () {
+    it('#save', function () {
+    });
+
+    it('#update', function () {
+    });
+
+    it('#validate', function(){
+    });
+
+    it('#invalidate', function(){
+    });
+
+    it('#delete', function(){
+    });
+
+    it('#isStillValid', function(){
+    });
+
+  });
+});

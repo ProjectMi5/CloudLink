561f003227625e1dc59b609921881683335239e9
Thomas Frei <mail@thomasfrei.de>
2015-11-22 17:01:23 +0100

    update config.sample and package.json for local testing scripts

--- config.sample.js
+++ config.sample.js
@@ -3,17 +3,47 @@
  */
 var config = {};
 
-config.GoogleAPI = '';
-config.HTTPPort = 3001;
-config.MQTTHost = 'mqtt://localhost';
-config.MongoDBHost = 'mongodb://localhost/test';
+// Secure config
 config.basicAuthUser = 'foo';
 config.basicAuthPW = 'bar';
+config.GoogleAPI = '';
 
-config.OrderHandling = {}; //stays empty
-config.OrderHandling.withoutApproval = []; //array of marketPlaceIds
+// more public config
+config.HTTPPort = 3001;
+config.MQTTHost = 'mqtt://mi5.itq.de';
+config.MongoDBHost = 'mongodb://localhost/test';
+
+config.OrderHandling = {};
+config.OrderHandling.withoutApproval = ['mi5', 'EU'];
 config.OrderHandling.prioritySettings = {
     standard    : 3,
+    EU          : 1
 };
+config.Cocktails = [10051,10053,10055,10060,10056,10057];
+config.Cookies = [10017,10018,10019];
+
+// Override some configuration for local tests
+if(process.env.TEST){
+    // Secure config
+    config.basicAuthUser = 'foo';
+    config.basicAuthPW = 'bar';
+    config.GoogleAPI = '';
+
+    // more public config
+    config.HTTPPort = 3001;
+    config.MQTTHost = 'mqtt://localhost';
+
+    config.MongoDBPath = '"C:\\Program Files\\MongoDB\\Server\\3.0\\bin\\mongod.exe" --dbpath C:\\Thomas\\mi5\\mongodb';
+    // Start Mongo DB
+    var exec = require('child_process').exec;
+    console.log('starting mongoDB', config.MongoDBPath);
+    exec(config.MongoDBPath, function(error, stdout, stderr){
+        console.log('ChildProcess'.red, 'stdout:', stdout, 'stderr:', stderr);
+        if (error !== null) {
+            console.log('ChildProcess'.red, 'exec error: ' + error);
+        }
+    });
+    config.MongoDBHost = 'mongodb://localhost/test';
+}
 
 module.exports = config;

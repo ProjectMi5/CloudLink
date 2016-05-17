5e60f4fe5fcf8f4aa25f8dc98ec9b49afc8646a5
Crzz <christoph@weinisch.com>
2016-05-17 13:42:53 +0200

    Add files via upload
    jsdoc app.js

--- app.js
+++ app.js
@@ -1,53 +1,103 @@
 /**
  * Encapsulated functionality
  */
+/**
+ * This class requires the modules {@link module:./config.js}
+ * @requires module:./config.js
+ */
 var config = require('./config.js');
+/** @requires module:./models/database.js*/
 var database = require('./models/database.js');
+/** @requires module:./models/google-cloud-messaging.js*/
 var gcm = require('./models/google-cloud-messaging.js');
-
+/** @requires module:q*/
 var Q = require('q');
 
-// MQTT
+
+/**
+ * MQTT and get client
+ * @requires module:mqtt
+ */
 var mqtt = require('mqtt');
+/**
+ * set client by {@link module:mqtt}
+ * @param {} config.MQTTHost
+ */
 var client  = mqtt.connect(config.MQTTHost);
 
 // -----------------------------------------------------------------
 // REST API
+/** @requires module:express*/
 var express = require('express');
+/** = express()
+ *  @param {} express
+ */
 var app = express();
+/** @requires module:http*/
 var server = require('http').Server(app);
+/** @requires module:socket.io*/
 var io = require('socket.io')(server);
 module.exports.io = io;
-
+/** @requires module:body-parser*/
 var bodyParser = require('body-parser');
+/** @requires module:basic-auth-connect*/
 var basicAuth = require('basic-auth-connect');
 
-// Form Data
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+/*
+ *Form Data
+ *parsing application/json
+ */
+app.use(bodyParser.json());
+/*
+ * parsing application/x-www-form-urlencoded
+ */
+app.use(bodyParser.urlencoded({extended: true}));
 
-// Static files:
+/*
+ * Static files:
+ */
 app.use(express.static(__dirname + '/public'));
 
-// View engine
+/*
+ * View engine
+ */
 app.set('views', __dirname+'/views');
 app.set('view engine', 'jade');
-
+/*
+ * Load Controlers
+ */
+/*
+ * @requires module:./controllers/device-handling
+ */
 var DeviceHandling  = require('./controllers/device-handling').DeviceHandling;
+/** @requires module:./controllers/recipe-handling*/
 var RecipeHandling  = require('./controllers/recipe-handling').RecipeHandling;
+/** @requires module:./controllers/order-handling*/
 var OrderHandling   = require('./controllers/order-handling').OrderHandling;
+/** @requires module:./controllers/feedback-handling*/
 var FeedbackHandling= require('./controllers/feedback-handling').FeedbackHandling;
+/** @requires module:./controllers/voucher-handling*/
 var VoucherHandling = require('./controllers/voucher-handling').VoucherHandling;
+/** @requires module:./controllers/machine-data-handling*/
 var MachineDataHandling = require('./controllers/machine-data-handling').MachineDataHandling;
-var CloudlinkHandling = require('./controllers/cloudlink-handling'); //other structure than other ctrls, better syntax highliting
+/**
+ * other structure than other ctrls, better syntax highliting
+ *  @requires module:./controllers/cloudlink-handling
+ */
+var CloudlinkHandling = require('./controllers/cloudlink-handling');
 
-// Routes: No authentification
+/*
+Routes: No authentification
+ */
 app.get('/helloWorld', function (req, res) {
     console.log('Hello World!');
     res.send('Hello World!');
 });
 
-// placeOrder without authentification
+/* placeOrder without authentification
+ * @param {String} =/QR/:identifier/:humanReadable
+ * @param {} = OrderHandling.placeOrderQR
+ */
 app.get('/QR/:identifier/:humanReadable', OrderHandling.placeOrderQR);
 
 // Basic authentification
@@ -115,9 +165,16 @@ server.listen(config.HTTPPort);
 // -----------------------------------------------------------------
 // -----------------------------------------------------------------
 // MQTT
-
+/**
+ * ServerPush path
+ * @const mi5ServerPush
+  */
 var mi5ServerPush = '/mi5/server/push';
 client.subscribe(mi5ServerPush);
+/**
+ * ServerUpstream path
+ * @const mi5ServerUpstream
+ */
 var mi5ServerUpstream = '/mi5/server/upstream';
 client.subscribe(mi5ServerUpstream);
 
@@ -148,20 +205,20 @@ client.on('message', function (topic, message) {
         message = JSON.parse(message);
 	      console.log(message);
 
-        // Check if it is a upstream-demo-message
+        /*Check if it is a upstream-demo-message*/
         if(typeof message.data.watchout != 'undefined'){
-            // Upstream-Demo sends the payload as JSON (gcm is JSON, and payload is JSON again)
+            /* Upstream-Demo sends the payload as JSON (gcm is JSON, and payload is JSON again)*/
             var payload = JSON.parse(message.data.watchout);
             console.log('payload:', payload);
             if(typeof payload.recommendation != 'undefined'){
                 console.log('message is a recommendation');
-                // cannot send JSON-object directly!
+                /*cannot send JSON-object directly!*/
                 client.publish('/mi5/showcase/cocktail/operator/recommendation', JSON.stringify(payload));
             }
         }
     }
 
-    // Forward the Feedback from the HMI
+    /* Forward the Feedback from the HMI*/
     if(mi5ShowcaseCocktailUserFeedback == topic){
         console.log('user feedback');
         //var payload = JSON.parse(message);

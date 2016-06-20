3e76e2c64bdbfaf1a960c0a203779f8f200c1743
Dominik Serve <serve@itq.de>
2016-06-20 14:31:59 +0200

    added new recipes for automatica

--- config.sample.js
+++ config.sample.js
@@ -19,7 +19,7 @@ config.OrderHandling.prioritySettings = {
     standard    : 3,
     EU          : 1
 };
-config.Cocktails = [10051,10053,10055,10060,10056,10057];
+config.Cocktails = [10051,10053,10055,10060,10061,10062,10063,10065,10066,10067,10070,10071,10072,10056,10057];
 config.Cookies = [10017,10018,10019];
 
 // Override some configuration for local tests
